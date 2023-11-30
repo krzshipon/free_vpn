5f0368495e66b5971013677e7480ecbb1986a661
krzshipon <khairuzzaman.shipon@bjitgroup.com>
2023-11-30 12:44:52 +0600

    [Ip Details] Embedded children in list view
     - Except header

--- lib/app/modules/ip_details/views/ip_details_view.dart
+++ lib/app/modules/ip_details/views/ip_details_view.dart
@@ -34,208 +34,221 @@ class IpDetailsView extends GetView<IpDetailsController> {
             title: "IP Info",
           ),
           verticalSpaceLarge,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(5.0), //or 15.0
-                child: Obx(
-                  () => Container(
-                    decoration: BoxDecoration(
-                      color: (controller
-                                  .homeController.ipDetails.value.countryCode ==
-                              null)
-                          ? Colors.red.shade300
-                          : null,
-                      border: Border.all(color: Get.theme.colorScheme.tertiary),
-                    ),
-                    height: 55.0,
-                    width: 80.0,
-                    child: (controller
-                                .homeController.ipDetails.value.countryCode ==
-                            null)
-                        ? const CsIcon(
-                            Icons.flag,
-                            color: Colors.white,
-                          )
-                        : Image.asset(
-                            'assets/flags/${controller.homeController.ipDetails.value.countryCode!.toLowerCase()}.png'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          verticalSpaceRegular,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Obx(
-                () => CSText(
-                    'Your IP: ${controller.homeController.ipDetails.value.query ?? 'fetching...'}'),
-              ),
-            ],
-          ),
-          verticalSpaceRegular,
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                child: CSCard(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    Container(
-                      color: Get.theme.colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CsIcon(
-                              Icons.home,
-                              color: Get.theme.colorScheme.onPrimary,
-                            ),
-                            horizontalSpaceTiny,
-                            CSText.title(
-                              'ISP',
-                              color: Get.theme.colorScheme.onPrimary,
-                            ),
-                            horizontalSpaceTiny
-                          ],
-                        ),
-                      ),
-                    ),
-                    verticalSpaceRegular,
-                    CSText(controller.homeController.ipDetails.value.isp ??
-                        'fetching...'),
-                    verticalSpaceRegular
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                child: CSCard(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    Container(
-                      color: Get.theme.colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CsIcon(
-                              Icons.location_on_outlined,
-                              color: Get.theme.colorScheme.onPrimary,
-                            ),
-                            horizontalSpaceTiny,
-                            CSText.title(
-                              'Location',
-                              color: Get.theme.colorScheme.onPrimary,
-                            ),
-                            horizontalSpaceTiny
-                          ],
-                        ),
-                      ),
-                    ),
-                    verticalSpaceRegular,
-                    Obx(() => CSText(_getLocation())),
-                    verticalSpaceRegular
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                child: CSCard(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    Container(
-                      color: Get.theme.colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CsIcon(
-                              Icons.access_time,
-                              color: Get.theme.colorScheme.onPrimary,
-                            ),
-                            horizontalSpaceTiny,
-                            CSText.title(
-                              'Time Zone',
-                              color: Get.theme.colorScheme.onPrimary,
-                            ),
-                            horizontalSpaceTiny
-                          ],
-                        ),
-                      ),
-                    ),
-                    verticalSpaceRegular,
-                    CSText(controller.homeController.ipDetails.value.timezone ??
-                        'fetching...'),
-                    verticalSpaceRegular
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                child: CSCard(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    Container(
-                      color: Get.theme.colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CsIcon(
-                              Icons.local_post_office,
-                              color: Get.theme.colorScheme.onPrimary,
-                            ),
-                            horizontalSpaceTiny,
-                            CSText.title(
-                              'Zip Code',
-                              color: Get.theme.colorScheme.onPrimary,
-                            ),
-                            horizontalSpaceTiny
-                          ],
-                        ),
-                      ),
-                    ),
-                    verticalSpaceRegular,
-                    CSText(controller.homeController.ipDetails.value.zip ??
-                        'fetching...'),
-                    verticalSpaceRegular
-                  ],
-                ),
-              ),
-            ],
-          ),
-          verticalSpaceSmall,
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
               children: [
-                Expanded(
-                  child: CSText.label(
-                    '*Thanks to vpn gate for providing openvpn servers to ip-api for providing ip details info.',
-                    textAlign: TextAlign.center,
-                    color: Colors.blueGrey,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(5.0), //or 15.0
+                      child: Obx(
+                        () => Container(
+                          decoration: BoxDecoration(
+                            color: (controller.homeController.ipDetails.value
+                                        .countryCode ==
+                                    null)
+                                ? Colors.red.shade300
+                                : null,
+                            border: Border.all(
+                                color: Get.theme.colorScheme.tertiary),
+                          ),
+                          height: 55.0,
+                          width: 80.0,
+                          child: (controller.homeController.ipDetails.value
+                                      .countryCode ==
+                                  null)
+                              ? const CsIcon(
+                                  Icons.flag,
+                                  color: Colors.white,
+                                )
+                              : Image.asset(
+                                  'assets/flags/${controller.homeController.ipDetails.value.countryCode!.toLowerCase()}.png'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                verticalSpaceRegular,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Obx(
+                      () => CSText(
+                          'Your IP: ${controller.homeController.ipDetails.value.query ?? 'fetching...'}'),
+                    ),
+                  ],
+                ),
+                verticalSpaceRegular,
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: CSCard(
+                        padding: EdgeInsets.zero,
+                        children: [
+                          Container(
+                            color: Get.theme.colorScheme.primary,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  CsIcon(
+                                    Icons.home,
+                                    color: Get.theme.colorScheme.onPrimary,
+                                  ),
+                                  horizontalSpaceTiny,
+                                  CSText.title(
+                                    'ISP',
+                                    color: Get.theme.colorScheme.onPrimary,
+                                  ),
+                                  horizontalSpaceTiny
+                                ],
+                              ),
+                            ),
+                          ),
+                          verticalSpaceRegular,
+                          CSText(
+                              controller.homeController.ipDetails.value.isp ??
+                                  'fetching...'),
+                          verticalSpaceRegular
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: CSCard(
+                        padding: EdgeInsets.zero,
+                        children: [
+                          Container(
+                            color: Get.theme.colorScheme.primary,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  CsIcon(
+                                    Icons.location_on_outlined,
+                                    color: Get.theme.colorScheme.onPrimary,
+                                  ),
+                                  horizontalSpaceTiny,
+                                  CSText.title(
+                                    'Location',
+                                    color: Get.theme.colorScheme.onPrimary,
+                                  ),
+                                  horizontalSpaceTiny
+                                ],
+                              ),
+                            ),
+                          ),
+                          verticalSpaceRegular,
+                          Obx(() => CSText(_getLocation())),
+                          verticalSpaceRegular
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: CSCard(
+                        padding: EdgeInsets.zero,
+                        children: [
+                          Container(
+                            color: Get.theme.colorScheme.primary,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  CsIcon(
+                                    Icons.access_time,
+                                    color: Get.theme.colorScheme.onPrimary,
+                                  ),
+                                  horizontalSpaceTiny,
+                                  CSText.title(
+                                    'Time Zone',
+                                    color: Get.theme.colorScheme.onPrimary,
+                                  ),
+                                  horizontalSpaceTiny
+                                ],
+                              ),
+                            ),
+                          ),
+                          verticalSpaceRegular,
+                          CSText(controller
+                                  .homeController.ipDetails.value.timezone ??
+                              'fetching...'),
+                          verticalSpaceRegular
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: CSCard(
+                        padding: EdgeInsets.zero,
+                        children: [
+                          Container(
+                            color: Get.theme.colorScheme.primary,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  CsIcon(
+                                    Icons.local_post_office,
+                                    color: Get.theme.colorScheme.onPrimary,
+                                  ),
+                                  horizontalSpaceTiny,
+                                  CSText.title(
+                                    'Zip Code',
+                                    color: Get.theme.colorScheme.onPrimary,
+                                  ),
+                                  horizontalSpaceTiny
+                                ],
+                              ),
+                            ),
+                          ),
+                          verticalSpaceRegular,
+                          CSText(
+                              controller.homeController.ipDetails.value.zip ??
+                                  'fetching...'),
+                          verticalSpaceRegular
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                verticalSpaceSmall,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: CSText.label(
+                          '*Thanks to vpn gate for providing openvpn servers to ip-api for providing ip details info.',
+                          textAlign: TextAlign.center,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+                verticalSpaceMedium
               ],
             ),
           ),
